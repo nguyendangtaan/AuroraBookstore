b101c91b63c94f336275e9d19afc12e6b12c1426
nguyendangtaan <dangtan236@gmail.com>
2024-11-17 15:00:48 +0700

    done baner

--- frontend/src/pages/users/homepage/index.jsx
+++ frontend/src/pages/users/homepage/index.jsx
@@ -1,8 +1,34 @@
 import {memo} from "react";
+import {Carousel,Image,Container,Row} from "react-bootstrap";
+import "./style.scss";
+import images from '../../../assets/users/img_baner.js';
+
+
 
 const Hompage=()=>{
     return(
-        <h1> heheh nhóm 20 </h1>
+    <Container fluid  className="home-page">
+    <Row> 
+        <Carousel className="pt-3">
+        <Carousel.Item className="carousel-item" interval={2000}>
+          <Image  src={images.image1} className=" baner img" /> 
+          <Carousel.Caption>   
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item interval={2000}>
+        <Image src={images.image2} className=" baner "  /> 
+          <Carousel.Caption>
+           
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item >
+        <Image src={images.image3} className=" baner "  /> 
+          <Carousel.Caption>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+      </Row> 
+      </Container>
     )
 }
 export default memo(Hompage)
