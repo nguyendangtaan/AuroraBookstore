cb362a422c197c185e530078127dcbca1ef1aa4c
j3un3t0nas <edgarust24@gmail.com>
2024-11-19 16:58:48 +0700

    raw login-signup frontend

--- frontend/src/ultils/router.jsx
+++ frontend/src/ultils/router.jsx
@@ -1,6 +1,7 @@
-export const ROUTERS={
-    USER :{
-        HOME:"",
-        CATALOG:"catalog"
-    }
-}
+export const ROUTERS = {
+  USER: {
+    HOME: "",
+    CATALOG: "catalog",
+    LOGIN: "login",
+  },
+};
