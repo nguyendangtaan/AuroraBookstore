f8b4428989f90602cef13497ec6e514557d98c34
nguyendangtaan <dangtan236@gmail.com>
2024-11-20 14:18:05 +0700

    done UI/UX mainpage

---
+++ frontend/src/assets/users/img.js
@@ -0,0 +1,26 @@
+const images = {
+    image1: require('./img_baner/1.png'),
+    image2: require('./img_baner/2.png'),
+    image3: require('./img_baner/3.png'),
+    image4: require('./img_baner/4.jpg'),
+    image5: require('./img_baner/5.jpg'),
+    image6: require('./img_baner/6.jpg'),
+ 
+  };
+  export const images_author={
+    image1: require('./img_author/1.jpg'),
+    image2: require('./img_author/2.jpg'),
+    image3: require('./img_author/3.jpg'),
+    image4: require('./img_author/4.jpg'),
+    image5: require('./img_author/5.jpg'),
+    image6: require('./img_author/6.jpeg'),
+    image7: require('./img_author/7.png'),
+    image8: require('./img_author/8.png'),
+    image9: require('./img_author/9.png'),
+    image10: require('./img_author/10.png'),
+    image11: require('./img_author/11.png'),
+    image12: require('./img_author/12.png'),
+  }
+  
+  
+  export default images;
--- frontend/src/assets/users/img_baner.js
+++
@@ -1,7 +0,0 @@
-const images = {
-    image1: require('./img_baner/1.png'),
-    image2: require('./img_baner/2.png'),
-    image3: require('./img_baner/3.png'),
-  };
-  
-  export default images;
